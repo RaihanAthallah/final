fa0fe9fb1b3c7db7ff58638fb2cb47faea03c778
Raihan Athallah <72775603+RaihanAthallah@users.noreply.github.com>
2023-06-21 18:40:22 +0700

    Update cookie.go

--- client/cookie.go
+++ client/cookie.go
@@ -20,7 +20,7 @@ func GetClientWithCookie(token string, cookies ...*http.Cookie) (*http.Client, e
 	jar.SetCookies(&url.URL{
 		Scheme: "https",
 		// uncomment this line if you want to use the production server
-		Host:   "final-production-2bfa.up.railway.app",
+		Host:   "tasktrack.up.railway.app",
 		// Host: "localhost:8080",
 	}, cookies)
 
