b1f51e0c55f90cc0def92fc3a64645b696a0de88
Raihan Athallah <72775603+RaihanAthallah@users.noreply.github.com>
2023-06-23 21:07:54 +0700

    Update cookie.go

--- client/cookie.go
+++ client/cookie.go
@@ -21,7 +21,7 @@ func GetClientWithCookie(token string, cookies ...*http.Cookie) (*http.Client, e
 
 		// uncomment this line if you want to use the production server
 		Scheme: "https",
-		Host:   "tasktrack.up.railway.app",
+		Host:   "mytasktracker.up.railway.app",
 
 		// Scheme: "http",
 		// Host:   "localhost:8080",
